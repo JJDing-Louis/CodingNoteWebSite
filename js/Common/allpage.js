1e4c5033941be4b2f6e8c01f83da4ed250e7b2d7
Jau Yi Ding <louis70121@livemail.tw>
2023-05-16 01:51:33 +0800

    Update

--- js/Common/allpage.js
+++ js/Common/allpage.js
@@ -1,16 +1,9 @@
-//
-function myFunction() {
-    var x = document.getElementById("demo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
-  
-  $(document).ready(function(){
-    $('header').load('header.html');
-    $('footer').load('footer.html');
-  });
+ 
+$(document).ready(function()
+{
+  $('header').load('header.html');
+  $('footer').load('footer.html');
+  $('aside').load('aside.html');
+});
   
   
